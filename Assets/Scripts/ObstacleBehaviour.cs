bec3ec25458fc1627a01e4428ae6f98c8b7a5e25
TanDN <tandoan0912@gmail.com>
2022-11-08 10:51:48 +0700

    change difficult

--- Assets/Scripts/ObstacleBehaviour.cs
+++ Assets/Scripts/ObstacleBehaviour.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleBehaviour : MonoBehaviour {
-	
+public class ObstacleBehaviour : MonoBehaviour
+{
 	[SerializeField] private float moveSpeed;
-
-	void Update () {
-		if(GameManager.Instance.GameState()){
+	public GameObject upObs;
+	public GameObject downObs;
+	void Update()
+	{
+		if (GameManager.Instance.GameState())
+		{
 			// Continuosly move the obstacles to the left if the game hasn't ended
 			transform.position = new Vector2(transform.position.x - Time.deltaTime * moveSpeed, transform.position.y);
 		}
--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -6,8 +6,9 @@ public class ObstacleSpawner : MonoBehaviour {
 
 	[SerializeField] private float waitTime;
 	[SerializeField] private GameObject[] obstaclePrefabs;
+	private int[] pattern = new int[] { 1, 1, 1, 0, 0 };
 	private float tempTime;
-
+	private int index = 0;
 	void Start(){
 		tempTime = waitTime - Time.deltaTime;
 	}
@@ -16,9 +17,16 @@ public class ObstacleSpawner : MonoBehaviour {
 		if(GameManager.Instance.GameState()){
 			tempTime += Time.deltaTime;
 			if(tempTime > waitTime){
+				if(index  > pattern.Length)
+				{
+					index = 0;
+				}
 				// Wait for some time, create an obstacle, then set wait time to 0 and start again
 				tempTime = 0;
-				GameObject pipeClone = Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)], transform.position, transform.rotation);
+                GameObject pipeClone = Instantiate(obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)], transform.position, transform.rotation);
+				pipeClone.GetComponent<ObstacleBehaviour>().upObs.transform.localScale = pattern[index] == 1 ? new Vector3(5f, 4.5f, 5f) : new Vector3(5f, 5f, 5f);
+                pipeClone.GetComponent<ObstacleBehaviour>().upObs.transform.localScale = pattern[index] == 1 ? new Vector3(5f, 4.5f, 5f) : new Vector3(5f, 5f, 5f);
+                index++;
 			}
 		}
 	}
